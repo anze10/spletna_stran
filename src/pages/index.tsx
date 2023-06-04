48bd61cef9b8512ea5cfd187d23d08f1dac311bf
anze <anze.repse@gmail.com>
2023-06-04 13:19:35 +0200

    prva strn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,81 @@
 import Navbar from 'src/components/Navbar';
-import { Box, Typography } from "@mui/material";
+import { Box, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import style from 'styled-jsx/style';
 
 function App() {
-    return (
-        <Box sx={{ height: '100%', width: '100%' }} >
-            <Navbar />
-            <Box>
-                <Typography sx={{ height: '30%', width: '30%' }} textAlign="left">
+    return <>
+        <Navbar />
+        <Box sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center' }} >
+            <Box sx={{ height: '100%', width: '1200px' }}  >
+
+                <br />
+                <Typography  >
                     Spoštovani,
                 </Typography >
-            </Box>
-            <Box >
 
-                <Typography sx={{ height: '30%', width: '40%' }} textAlign="left">
+
+                <Typography >
                     Društvo je prostovoljno združenje strokovnjakov varnosti in zdravja pri delu,
                     strokovnjakov področja varstva pred požarom, varstva okolja ter ostalih, predvsem iz naše regije,
                     z dolžnostjo, da spoštujejo določbe statuta društva, kodeks stroke in varujejo ugled društva.
                     Društvo je povezano z ostalimi društvi v ZVIS Slovenije.
                 </Typography >
-            </Box>
-            <br />
-            <Box>
-                <Typography sx={{ height: '30%', width: '30%' }} textAlign="left">
+
+                <br />
+
+                <Typography >
                     Namen in cilji so:
                 </Typography >
-            </Box>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Informiranje, usposabljanje in povezovanje strokovnjakov v obliki posvetov, seminarjev in strokovnih ekskurzij, `} />
+                </ListItem>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Sodelovanje pri pripravi zakonov in podzakonskih aktov,  `} />
+                </ListItem>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Nudenje pomoči članom,`} />
+                </ListItem>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Povezovanje s podobnimi asociacijami, `} />
+                </ListItem>
+                <br />
+                <Typography >
+                    Organi društva so:
+                </Typography >
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Občni zbor,  `} />
+                </ListItem>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Izvršilni odbor,`} />
+                </ListItem>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Nadzorni odbor `} />
+                </ListItem>
+                <ListItem component="div" disablePadding>
+                    <ListItemText primary={`Disciplinska komisija  `} />
+                </ListItem>
+                <br />
+                <Typography>
+                    S članstvom v Evropski uniji nas čakajo novi izzivi, saj varnost in zdravje, kakor tudi ekologija postajata vse pomembnejši vrednoti, ki vplivata na kakovost življenja in dela, na uspešnost poslo vanja podjetij in vseh ljudi.
+                </Typography >
+                <br />
+                <Typography>
+                    MDVI Novo mesto
+                </Typography >
+                <Typography >
+                    Predsednik:
+                </Typography >
+                <Typography >
+                    Miro ŠKUFCA, univ. dipl. inž.
+                </Typography >
 
+
+
+
+            </Box>
         </Box>
 
-    )
+
+    </>
 }
 export default App 
